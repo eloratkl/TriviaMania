97967956b3809bb4cec120021cf21108e2cd2c2a
catherine.tiong <catherine.tiong@fdmgroup.com>
2023-10-05 14:44:31 +0800

    adding login, register and logout features

---
+++ trivia-mania/src/components/structure/RenderNavigation.js
@@ -0,0 +1,61 @@
+import { Link, Route, Routes } from "react-router-dom";
+import { AuthData } from "../../auth/AuthWrapper";
+import { nav } from "./navigation";
+
+export const RenderRoutes = () => {
+  const { user } = AuthData();
+
+  return (
+    <Routes>
+      {nav.map((r, i) => {
+        if (r.isPrivate && user.isAuthenticated) {
+          return <Route key={i} path={r.path} element={r.element} />;
+        } else if (!r.isPrivate) {
+          return <Route key={i} path={r.path} element={r.element} />;
+        } else return null;
+      })}
+    </Routes>
+  );
+};
+
+export const RenderMenu = () => {
+  const { user, logout } = AuthData();
+
+  const MenuItem = ({ r }) => {
+    return (
+      <div className="menuItem">
+        <Link to={r.path}>{r.name}</Link>
+      </div>
+    );
+  };
+
+  const hasRegisterLink = nav.some((r) => r.path === "/register");
+
+  return (
+    <div className="menu">
+      {nav.map((r, i) => {
+        if ((!r.isPrivate && r.isMenu) || (r.path === "/register" && !user.isAuthenticated)) {
+          return <MenuItem key={i} r={r} />;
+        } else if (user.isAuthenticated && r.isMenu) {
+          return <MenuItem key={i} r={r} />;
+        } else return null;
+      })}
+
+      {user.isAuthenticated ? (
+        <div className="menuItem" onClick={logout}>
+          Logout
+        </div>
+      ) : (
+        <div className="menuItem">
+          <Link to="/login">Login</Link>
+        </div>
+      )}
+
+      {!user.isAuthenticated && !hasRegisterLink && (
+        <div className="menuItem">
+          <Link to="/register">Register</Link>
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ trivia-mania/src/pages/About.js
@@ -0,0 +1,8 @@
+export const About = () => {
+  return (
+    <div className="page">
+      <h2>About page</h2>
+      <p>This is the text for the about page</p>
+    </div>
+  );
+};
